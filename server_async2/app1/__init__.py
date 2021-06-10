a71629b875ffdd9a88a6c658ed5c8a6f4fc99193
xenu_xore <door.xore@gmail.com>
2021-06-10 14:48:42 +0300

    add static data

--- server_async2/app1/__init__.py
+++ server_async2/app1/__init__.py
@@ -4,7 +4,8 @@ import aiohttp_jinja2
 import base64
 from cryptography import fernet
 from aiohttp import web
-from aiohttp_session import setup, get_session, session_middleware
+
+from aiohttp_session import setup, SimpleCookieStorage
 from aiohttp_session.cookie_storage import EncryptedCookieStorage
 
 TEMPLATE_PATH = os.path.join(os.path.dirname(__file__), "templates")
@@ -15,22 +16,21 @@ from views_app1.views import web_app1
 
 
 async def make_app():
+
+    # генерация ключей для кук
+    fer_key = fernet.Fernet.generate_key()
+    secret_key = base64.urlsafe_b64decode(fer_key)
+
+    # приложение и надстройки над ним
     app = web.Application()
-
+    setup(app, EncryptedCookieStorage(secret_key, cookie_name="COOKIES"))
     aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(TEMPLATE_PATH))
+    app['static_root_url'] = '/static'
 
-
-    # secret_key must be 32 url-safe base64-encoded bytes
-    fernet_key = fernet.Fernet.generate_key()
-    secret_key = base64.urlsafe_b64decode(fernet_key)
-    setup(app, EncryptedCookieStorage(secret_key))
+    # роутинг путей
+    app.router.add_static('/static/', STATIC_PATH, name='static')
     app.add_routes(web_app1)
     app.add_routes(web_app2)
+
     return app
-
-
-
-
-
-
 
--- server_async2/app1/views_app1/views.py
+++ server_async2/app1/views_app1/views.py
@@ -1,6 +1,7 @@
 from server_async2.app1 import web
 import aiohttp_jinja2
 from aiohttp_session import get_session, new_session
+import time
 
 web_app1 = web.RouteTableDef()
 
@@ -8,10 +9,13 @@ web_app1 = web.RouteTableDef()
 @web_app1.get('/', name="home")
 @aiohttp_jinja2.template('home.html')
 async def get_handler(request):
-    location_login = request.app.router["do_login"].url_for()
 
+    location_login = request.app.router["do_login"].url_for()
+    print(location_login)
     session = await get_session(request)
-    return {'url_login': location_login, 'session_login': session}
+    session['last_visit'] = time.time()
+
+    return {}
 
 
 @web_app1.get('/page', name='page_test')
